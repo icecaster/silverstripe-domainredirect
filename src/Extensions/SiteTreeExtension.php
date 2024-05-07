bad832132497c735df953443581e1a6d186c3b6f
Tim Klein <tim@dodat.co.nz>
2024-05-07 15:43:58 +1200

    Handling proxies (cloudflare)

--- src/Extensions/SiteTreeExtension.php
+++ src/Extensions/SiteTreeExtension.php
@@ -19,17 +19,25 @@ class SiteTreeExtension extends Extension
         if($base_url = Environment::getEnv($varname)) {
             
             $base_url = rtrim($base_url, "/"); //remove trailing slashes
+
+            $current_scheme = isset($_SERVER['HTTP_X_FORWARDED_PROTO']) ? 
+                $_SERVER['HTTP_X_FORWARDED_PROTO'] : $_SERVER['REQUEST_SCHEME'];
+
+            $current_host = $_SERVER['SERVER_NAME'];
             
-            $current_url = $_SERVER['REQUEST_SCHEME']."://".$_SERVER['SERVER_NAME'];
-            
-            if(!str_starts_with($base_url, $current_url)) {
-                $target_url = $base_url.$_SERVER['REQUEST_URI'];
+            $current_url = "{$current_scheme}://{$current_host}";
+
+                        
+            if(!str_starts_with($base_url, $current_url) && $base_url !== $current_url) {
+                 $target_url = $base_url.$_SERVER['REQUEST_URI'];
                 
-                $controller->redirect($target_url, 301);
-                return;
+                 $controller->redirect($target_url, 301);
+                 return;
             }
+
         }
     }
 
 }
+
 
